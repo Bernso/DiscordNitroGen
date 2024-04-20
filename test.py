68aea29f5dc84057c0ef95563eb1ba6b1bc8558b
benwsheridan <ben.w.sheridan@gmail.com>
2024-04-20 22:24:57 +0100

    yes

--- test.py
+++ test.py
@@ -13,19 +13,21 @@ app.title("Nitro Gen by Bernso")
 
 
 def open_nitro(num1):
-    if statusLabel.cget('text') == f"BEANS {num1}":
-        webbrowser.open_new(f"https://discord.gift/{num1}")
+    if statusLabel.cget('text') == f"BEANS {num1}": # USING THIS METHOD DOES NOT WORK
+        webbrowser.open_new(f"https://discord.gift/{num1}") # USING THIS METHOD DOES NOT WORK
     else:
         print("No current code found, please wait and try again...")
 
 
 def startCode(): # change label
-    num1 = random.randint(1, 100)
-    if num1 == 70:
-        statusLabel.configure(text=f"BEANS {num1}")
-        open_nitro(num1)
-    else:
-        print("Invalid num1 value")
+    num1 = 0
+    while num1 != 70:
+        num1 = random.randint(1, 100)
+        if num1 == 70:
+            statusLabel.configure(text=f"BEANS {num1}")
+            open_nitro(num1)
+        else:
+            print("Invalid num1 value")
 
 statusLabel = tk.Label(app, text="Code has not been found yet.")
 statusLabel.bind("<Button-1>", open_nitro)
