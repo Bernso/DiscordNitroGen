2da2d599e65af9c61268661c7daa359eaadd9896
benwsheridan <ben.w.sheridan@gmail.com>
2024-04-20 22:33:12 +0100

    organising

--- withUI.py
+++ withUI.py
@@ -4,27 +4,33 @@ def main():
     while True:
         allChars = f'qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890'
         code = ''
+        
         for i in range(1, 19): # add 1 to the final value
             code += random.choice(allChars)
         # 18 charcters
+        
         url = f'https://www.discordapp.com/api/v9/entitlements/gift-codes/{code}'
         decider = requests.get(url)
+        
         if decider.status_code == 200:
             print(f'Code {code} is valid!')
             print(f'https://discord.gift/{code}')
             statusLabel.configure(text=f"Code found!\nhttps://discord.gift/{code}", font=('helvetica', 14, 'bold'))
             webbrowser.open_new(f"https://discord.gift/{code}")
             return code
+        
         else:
             print(f'Code {code} is not valid!')
+            
+            
 def startCode():
     startButton.destroy()
     main()
 
+
 def open_nitro(event):
     for i in range(1, 11):
         webbrowser.open_new(f"https://www.youtube.com/watch?v=dQw4w9WgXcQ")
-
 
 
 root = tk.Tk()
@@ -37,14 +43,6 @@ statusLabel.pack(padx = 20, pady = 20)
 
 startButton = tk.Button(root, text="Start", command=startCode)
 startButton.pack(padx = 20)
-
-
-
-
-
-
-
-
 
 
 
