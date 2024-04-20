e31da4278f3ef1153b04ff06170367d09fae911d
benwsheridan <ben.w.sheridan@gmail.com>
2024-04-20 22:05:09 +0100

    added main function

--- withUI.py
+++ withUI.py
@@ -1,5 +1,22 @@
 import tkinter as tk
 
+def main():
+    allChars = f'qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890'
+    code = ''
+    for i in range(1, 19): # add 1 to the final value
+        code += random.choice(allChars)
+    # 18 charcters
+    url = f'https://www.discordapp.com/api/v9/entitlements/gift-codes/{code}'
+    decider = requests.get(url)
+    if decider.status_code == 200:
+        print(f'Code {code} is valid!')
+        print(f'https://discord.gift/{code}')
+        input()
+    else:
+        print(f'Code {code} is not valid!')
+
+
+
 root = tk.Tk()
 root.geometry("300x300")
 root.title("Nitro Gen by Bernso")
